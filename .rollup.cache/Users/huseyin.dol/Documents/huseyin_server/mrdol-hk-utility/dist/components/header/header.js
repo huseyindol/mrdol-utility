51943c9deaaced7fbc82a1b3caaf8b33f02671ec
huseyindol-hg <huseyin.dol@hangikredi.com>
2023-10-24 13:42:46 +0300

    fix

--- .rollup.cache/Users/huseyin.dol/Documents/huseyin_server/mrdol-hk-utility/dist/components/header/header.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-const header = (props) => {
-    return (_jsx("header", { className: "bg-gray-400", children: _jsxs("div", { className: "container mx-auto flex bg-gray-700 text-white p-10", children: [_jsx("div", { className: "text-success", children: "Logo" }), _jsx("div", { className: "ml-auto text-info", children: "Menu" })] }) }));
-};
-export default header;
-//# sourceMappingURL=header.js.map
--- .rollup.cache/Users/huseyin.dol/Documents/huseyin_server/mrdol-hk-utility/types/components/header/header.d.ts
+++
@@ -1,3 +0,0 @@
-type Props = {};
-declare const header: (props: Props) => import("react/jsx-runtime").JSX.Element;
-export default header;
